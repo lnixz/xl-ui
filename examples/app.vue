9c3d311f692428c3c2e4bf6191f39f4affcb4b74
ZXinL-CN <xinliang.zhu@qq.com>
2023-04-15 18:35:20 +0800

    feature(): Empty the warehouse

--- examples/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <Button></Button>
-    </div>
-</template>
-
-<script lang="ts" setup>
-import { Button } from 'xinl-ui';
-</script>
--- packages/components/index.ts
+++
@@ -1,5 +0,0 @@
-import Button from './src/button'
-
-export {
-    Button,
-}
